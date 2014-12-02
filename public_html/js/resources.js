f84c965f2fb9f981795b6444e55ef04c76620cf1
Hrag Krikorian <hrago18@gmail.com>
2014-12-02 15:03:10 -0800

    still updating...

--- public_html/js/resources.js
+++ public_html/js/resources.js
@@ -8,6 +8,7 @@ game.resources = [
         {name: "Background-tiles", type:"image", src: "data/img/background-tiles.png"},
         {name: "Meta-tiles", type:"image", src: "data/img/meta-tiles.png"},
         {name: "Bruh", type:"image", src: "data/img/player1.png"},
+         {name: "title-screen", type:"image", src: "data/img/mario.png"},
 
 	/* Atlases 
 	 * @example
@@ -21,6 +22,7 @@ game.resources = [
  	 */
         {name: "HragK01", type: "tmx", src: "data/map/HragK01.tmx"},
         {name: "HragK02", type: "tmx", src: "data/map/HragK02.tmx"},
+        {name: "HragK03", type: "tmx", src: "data/map/HragK03.tmx"},
 
 	/* Background music. 
 	 * @example
